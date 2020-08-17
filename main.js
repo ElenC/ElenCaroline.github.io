a8e290ee52fa13fc3a5efda49a2f861f068f19bc
Elen Caroline <carolineelen98@gmail.com>
2020-08-17 16:33:02 -0300

    iniciando

---
+++ main.js
@@ -0,0 +1,20 @@
+
+    const acesso = new XMLHttpRequest()
+    acesso.open('GET', 'https://api.github.com/users/ElenC/repos')
+    
+    acesso.onreadystatechange = function(){
+
+        if(this.status == 200 && this.readyState == 4){
+
+            const main = document.querySelector('main')
+            const cont = JSON.parse(acesso.responseText)
+            
+            for(let i =0; i<cont.length; i++){
+
+                main.innerHTML += "<div id='container'><div>"+ cont[i].name + "<div id='name'><img id='img1' src='" + cont[i].name + 
+                ".png'></div><div id='html'><a href='"+ cont[i].html_url + "'target='_blank'><input type='button'id='button' value='ACESSAR'></a></div></div>"
+            }
+        }
+      
+    }
+    acesso.send()
