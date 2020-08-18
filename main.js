29ab314e2f82be9e9cbbc4571b70dd6665530768
Elen Caroline <carolineelen98@gmail.com>
2020-08-18 13:51:39 -0300

    continuacao projero

--- main.js
+++ main.js
@@ -12,7 +12,7 @@
             for(let i =0; i<cont.length; i++){
 
                 main.innerHTML += "<div id='container'><div>"+ cont[i].name + "<div id='name'><img id='img1' src='../imagens/" + cont[i].name + 
-                ".png'></div><div id='html'><a href='"+ cont[i].html_url + "'target='_blank'><input type='button'id='button' value='ACESSAR'></a></div></div>"
+                ".png'></div><div id='html'><a href='"+ cont[i].html_url + "'target='_blank'><input type='button'id='button' value='ACESSAR'></a></div><div><a id='detalhe' href='detalhes.html'>Mais Detalhes</div></div>"
             }
         }
       
