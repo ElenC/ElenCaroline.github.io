74658c1cd327c9cc614a919bd68c87e190717f14
Elen Caroline <carolineelen98@gmail.com>
2020-08-19 14:32:52 -0300

    incluido informacoes sobre projeto

--- main2.js
+++ main2.js
@@ -1,11 +1,10 @@
-var inf = document.querySelector('main')
+let inf = document.querySelector('main')
 
-var rep = JSON.parse(localStorage.getItem('detalhe'))
+let rep = JSON.parse(localStorage.getItem('detalhe'))
 
 if(rep == null){
     rep =[]
 } 
-
 
 const det = new XMLHttpRequest()
 det.open('GET', 'https://api.github.com/repos/ElenC/' + rep[0].name, false)
@@ -15,7 +14,7 @@ det.onreadystatechange = function(){
 
         let rep1 = JSON.parse(this.responseText)
     
-        inf.innerHTML += '<div><div>' + rep1.owner.login + '</div><div><img src="' + rep1.owner.avatar_url + '"></div><div>' 
+        inf.innerHTML += '<div id="container-det"><div id="login">' + rep1.owner.login + '</div><div><img id="img2" src="' + rep1.owner.avatar_url + '"></div><div id="descrip">' 
         + rep1.description + '</div></div>' 
         
     }
@@ -30,7 +29,7 @@ det.onreadystatechange = function(){
 
         let rep1 = JSON.parse(this.responseText)
     
-        inf.innerHTML += '<div>Seguindo: ' + rep1.length + ' Usuários</div>' 
+        inf.innerHTML += '<div><div id="follow">Seguindo: ' + rep1.length + ' Usuários</div></div>' 
         
     }
 }
@@ -45,7 +44,7 @@ det.onreadystatechange = function(){
         let rep1 = JSON.parse(this.responseText)
         for(let i=0; i<rep1.length; i++){
 
-        inf.innerHTML += '<div>' + rep1[i].commit.message + '</div>' 
+        inf.innerHTML += '<div><div id="msg">' + rep1[i].commit.message + '</div></div>' 
 
         }
     }
@@ -60,7 +59,7 @@ det.onreadystatechange = function(){
         let rep1 = JSON.parse(this.responseText)
         for(let i=0; i<rep1.length; i++){
 
-        inf.innerHTML += '<div>' + rep1[i].name+ '</div>' 
+        inf.innerHTML += '<div><div id="branch">' + rep1[i].name+ '</div></div>' 
 
         }
     }
@@ -75,7 +74,7 @@ det.onreadystatechange = function(){
         let rep1 = JSON.parse(this.responseText)
         for(prop in rep1){
 
-        inf.innerHTML += '<div>' + prop + '</div>' 
+        inf.innerHTML += '<div><div id="lang" >' + prop + '</div></div>' 
 
         }
     }
